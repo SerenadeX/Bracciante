8bdfce266b8be8ba41211e98cd4c4842e53d3de6
Rhett Rogers <serenade.xs@icloud.com>
2017-04-10 21:45:07 -0600

    Initial Commit

---
+++ Bracciante/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bracciante
+//
+//  Created by Rhett Rogers on 10/04/17.
+//  Copyright © 2017 LyokoTech. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
